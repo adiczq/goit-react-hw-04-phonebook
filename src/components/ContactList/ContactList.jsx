fd96f80bf072d38eac6aa04b36b98632ccb60a05
Adrian Lacheta <adiczq@gmail.com>
2023-07-11 16:23:47 +0200

    Create ContactList.jsx

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Contact from './Contact';
+import css from './ContactList.module.css';
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul className={css.list}>
+      {contacts.map(contact => (
+        <Contact
+          key={contact.id}
+          contact={contact}
+          onDeleteContact={onDeleteContact}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
